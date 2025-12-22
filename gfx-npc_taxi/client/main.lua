f7ed43061010008d2417453b3e0e147003c57478
Grafix <139239309+GFX-Code@users.noreply.github.com>
2025-12-22 20:52:24 +0000

    Add files via upload

--- gfx-npc_taxi/client/main.lua
+++ gfx-npc_taxi/client/main.lua
@@ -3,7 +3,7 @@ local function CallTaxi()
 end
 
 local taxiClass = require 'client.classes.taxi'
-local taxiInstance = nil 
+local taxiInstance = nil
 
 RegisterNetEvent('taxi-bot:client:callVehicle', function()
     local playerCoords = GetEntityCoords(PlayerPedId())
@@ -11,76 +11,160 @@ RegisterNetEvent('taxi-bot:client:callVehicle', function()
     if not taxiInstance then
         taxiInstance = taxiClass:new()
         taxiInstance:RequestTaxi(playerCoords)
-    else
-        if taxiInstance.state == "idle" then
-            taxiInstance:Cleanup()
-            Citizen.Wait(500)
-            taxiInstance:RequestTaxi(playerCoords)
-        else
-            lib.notify({
-                type = 'warning',
-                description = 'A taxi is already on the way!',
-                duration = 5000
-            })
-        end
+        return
     end
+
+    if taxiInstance.state == "idle" then
+        taxiInstance:Cleanup()
+        Wait(500)
+        taxiInstance:RequestTaxi(playerCoords)
+        return
+    end
+
+    lib.notify({
+        type = 'warning',
+        description = 'A taxi is already on the way!',
+        duration = 5000
+    })
 end)
 
 RegisterCommand("taxiRushMode", function()
     local playerPed = PlayerPedId()
+
     if taxiInstance
-        and taxiInstance.state == "in_trip" 
-        and taxiInstance.driver 
-        and DoesEntityExist(taxiInstance.driver) 
-        and IsPedInVehicle(playerPed, taxiInstance.vehicle, false) 
+        and taxiInstance.state == "in_trip"
+        and taxiInstance.driver
+        and DoesEntityExist(taxiInstance.driver)
+        and IsPedInVehicle(playerPed, taxiInstance.vehicle, false)
         and not taxiInstance.rushMode
     then
         taxiInstance.rushMode = true
-        lib.notify({type = "success", description = "Taxi is now driving faster!"})
+        lib.notify({
+            type = "success",
+            description = "Taxi is now driving faster!"
+        })
     end
 end, false)
 
 RegisterKeyMapping("taxiRushMode", "Speed up the taxi", "keyboard", "E")
 
-Citizen.CreateThread(function()
+CreateThread(function()
+    local uiShown = false
+
     while true do
-        Citizen.Wait(0)
+        Wait(1000) -- checks every 1000ms instead of every frame, thank you Randolio
+
         local playerPed = PlayerPedId()
-        if taxiInstance
+        local shouldShow = taxiInstance
             and taxiInstance.state == "in_trip"
             and taxiInstance.driver
             and DoesEntityExist(taxiInstance.driver)
             and IsPedInVehicle(playerPed, taxiInstance.vehicle, false)
             and not taxiInstance.rushMode
-        then
+
+        if shouldShow and not uiShown then
             lib.showTextUI("[E] Press to speed up")
-        else
+            uiShown = true
+        elseif not shouldShow and uiShown then
             lib.hideTextUI()
+            uiShown = false
+        end
+    end
+end)
+
+local spawnedPayphones = {} 
+
+CreateThread(function()
+    if not Config.PlacedPayphones then return end
+
+    for _, phone in pairs(Config.PlacedPayphones) do
+        RequestModel(phone.prop)
+        while not HasModelLoaded(phone.prop) do
+            Wait(0)
+        end
+
+        local obj = CreateObject(
+            phone.prop,
+            phone.coords.x,
+            phone.coords.y,
+            phone.coords.z - 1.0,
+            false,
+            false,
+            false
+        )
+
+        SetEntityHeading(obj, phone.coords.w)
+        FreezeEntityPosition(obj, true)
+        SetEntityInvincible(obj, true)
+
+        spawnedPayphones[obj] = true
+
+        exports.ox_target:addLocalEntity(obj, {
+            {
+                name = 'call_taxi_custom_phone',
+                icon = 'fa-solid fa-car',
+                label = 'Call Taxi',
+                distance = 2.5,
+                onSelect = CallTaxi
+            }
+        })
+
+        SetModelAsNoLongerNeeded(phone.prop)
+    end
+end)
+
+local worldPayphones = {}
+
+CreateThread(function()
+    local models = Config.PayphoneModels or {}
+
+    while true do
+        Wait(2000)
+
+        local playerCoords = GetEntityCoords(PlayerPedId())
+
+        for _, model in ipairs(models) do
+            local phone = GetClosestObjectOfType(
+                playerCoords.x,
+                playerCoords.y,
+                playerCoords.z,
+                3.0,
+                model,
+                false,
+                false,
+                false
+            )
+
+            if phone ~= 0
+                and not worldPayphones[phone]
+                and not spawnedPayphones[phone]
+            then
+                worldPayphones[phone] = true
+
+                exports.ox_target:addLocalEntity(phone, {
+                    {
+                        name = 'call_taxi_world_phone',
+                        icon = 'fa-solid fa-car',
+                        label = 'Call Taxi',
+                        distance = 2.5,
+                        onSelect = CallTaxi
+                    }
+                })
+            end
         end
     end
 end)
 
 AddEventHandler("onResourceStop", function(resName)
-    if resName == GetCurrentResourceName() and taxi then
-        taxi:Cleanup()
-    end
-end)
+    if resName ~= GetCurrentResourceName() then return end
 
-Citizen.CreateThread(function()
-    local payphoneModels = Config.PayphoneModels or {}
-    for _, model in ipairs(payphoneModels) do
-        if type(model) == "number" or type(model) == "string" then
-            exports.ox_target:addModel({model}, {
-                {
-                    name = 'call_taxi',
-                    icon = 'fa-solid fa-car',
-                    label = 'Call Taxi',
-                    distance = 2.5,
-                    onSelect = function()
-                        TriggerServerEvent('taxi:callFromPayphone')
-                    end
-                }
-            })
+    for obj,_ in pairs(spawnedPayphones) do
+        if DoesEntityExist(obj) then
+            DeleteEntity(obj)
         end
+    end
+
+    if taxiInstance then
+        taxiInstance:Cleanup()
     end
 end)
