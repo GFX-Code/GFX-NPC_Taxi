481448d53e04c1aca5b40b32fec788a3433d7b21
Grafix <139239309+GFX-Code@users.noreply.github.com>
2025-12-27 20:25:33 +0000

    Add files via upload

--- gfx-npc_taxi/server/main.lua
+++ gfx-npc_taxi/server/main.lua
@@ -2,35 +2,51 @@ RegisterNetEvent('taxi:callFromPayphone')
 AddEventHandler('taxi:callFromPayphone', function()
     local src = source
     if src > 0 then
-        TriggerClientEvent('taxi-bot:client:callVehicle', src)
+        TriggerClientEvent('taxi:client:callVehicle', src)
     end
+end)
+
+lib.callback.register('taxi:server:canAffordTaxi', function(source)
+    local src = source
+
+    if not Config.Framework.QBBox() then return false end
+
+    local player = exports['qbx_core']:GetPlayer(src)
+    if not player then return false end
+
+    local cash = player.PlayerData.money.cash or 0
+    local bank = player.PlayerData.money.bank or 0
+
+    return (cash + bank) >= Config.TaxiCallFee
 end)
 
 lib.callback.register('taxi:server:payForTaxi', function(source, price)
     local src = source
-    if Config.Framework.QBBox() then
-        local player = exports['qbx_core']:GetPlayer(src)
-        if player and player.PlayerData then
-            local paid = false
-            if player.Functions.RemoveMoney('cash', price, GetCurrentResourceName()) then
-                paid = true
-            elseif player.Functions.RemoveMoney('bank', price, GetCurrentResourceName()) then
-                paid = true
-            end
 
-            if paid then
-                return true
-            else
-                TriggerClientEvent('ox_lib:notify', src, {
-                    type = 'warning',
-                    description = 'Not enough money!',
-                    position = "center-right",
-                    duration = 5000
-                })
-                return false
-            end
-        end
+    if not Config.Framework.QBBox() then return false end
+
+    local player = exports['qbx_core']:GetPlayer(src)
+    if not player then return false end
+
+    price = tonumber(price)
+    if not price or price <= 0 then return true end
+
+    if player.Functions.RemoveMoney('cash', price, 'taxi') then
+        return true
     end
+
+    if player.Functions.RemoveMoney('bank', price, 'taxi') then
+        return true
+    end
+
+    lib.notify(src, {
+        type = 'warning',
+        description = 'Not enough money!',
+        position = 'center-right',
+        duration = 5000
+    })
+
+    return false
 end)
 
 AddEventHandler("onResourceStop", function(resName)
